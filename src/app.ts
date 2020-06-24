b1328745b39c2602edd109da029214dcac3b700f
Fabricio Tello <ftello@puntoticket.cl>
2020-06-24 19:55:38 -0400

    agrega socket.io

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,13 @@ import indexRoutes from './routes/index';
 import passport from 'passport';
 import passportMiddleware from './middlewares/passport';
 
-
 // inicializacion
 const app = express();
 
 
 // settings
 app.set('port', process.env.PORT || 4000);
+const http = require('http').Server(app);
 
 //middlewares
 app.use(morgan('dev'));
@@ -20,6 +20,14 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(passport.initialize());
 passport.use(passportMiddleware);
+
+
+const io = require('socket.io')(8081);
+
+io.on('connection', (socket:any) => {
+    console.log('usuario conectado al socket');
+    socket.emit('bienvenido', 'holanda');
+})
 
 // routes
 app.get('/', (req, res) => {
