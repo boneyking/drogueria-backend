aafa28c8db211cd8a0a3ea72adf02eef9606bac8
Fabricio Tello <ftello@puntoticket.cl>
2020-07-01 21:47:44 -0400

    incorpora nuevas rutas y modifica rutas actuales

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,9 +5,10 @@ import multer from "../libs/multer";
 import { obtenerArticulos, crearArticulo, verificarExistenciaCodigoBarra } from "../controllers/articulo.controller";
 
 import {ingresar, crearUsuarioInicial} from '../controllers/login.controller';
-import { crearArsenal, obtenerArsenalPaginado } from "../controllers/arsenal.controller";
+import { crearArsenal, obtenerArsenalPaginado, buscarArsenalPorNombre } from "../controllers/arsenal.controller";
 import { cargarProveedores, cargarArsenal } from "../controllers/data-inicial.controller";
 import { crearRecepcion } from "../controllers/recepcion.controller";
+import { buscarProveedorPorRut, buscarProveedorPorNombre } from "../controllers/proveedor.controller";
 
 const router = Router();
 
@@ -22,13 +23,16 @@ router.route('/login').post(ingresar);
 
 router.route('/crearUsuarioInicial').post(crearUsuarioInicial);
 
-
 router.route('/arsenal/crearArsenal').post(crearArsenal);
 router.route('/arsenal/obtenerArsenalPaginado',).post(obtenerArsenalPaginado);
+router.route('/arsenal/buscarArsenalPorNombre/:nombre').get(buscarArsenalPorNombre);
 
 router.route('/data-inicial/proveedor').post(cargarProveedores);
 router.route('/data-inicial/arsenal').post(cargarArsenal);
 
 router.route('/recepcion').post(crearRecepcion);
+
+router.route('/proveedor/buscarProveedorPorRut/:rut').get(buscarProveedorPorRut);
+router.route('/proveedor/buscarProveedorPorNombre/:nombre').get(buscarProveedorPorNombre);
 
 export default router;
